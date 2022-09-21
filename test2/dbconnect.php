bb77677a1962db07ac343d8cddf04b950f37a91e
BigMoon1781 <112223496+BigMoon1781@users.noreply.github.com>
2022-09-21 18:45:05 +0900

    課題提出テスト_bugTest2
    バグ修正内容の説明の添付と
    バグTest2の回答の送付

---
+++ test2/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "";
+// データベース名
+$db['dbname'] = "YIGroupBlog";	//2022/09/21 DB名が違っている
